d7da2be1a3dbbe9b0f78c25c91b24170ab7d88c7
Philip Thomsen <philip-th@live.dk>
2022-09-05 11:38:35 +0200

    Quicksave

---
+++ script.js
@@ -0,0 +1,72 @@
+//Punkt 1
+document.querySelector('#ex1').addEventListener('click', function () {
+document.querySelectorAll('h1')[0].innerText = "Heiiiii det mig"    
+})
+
+//Punkt 2
+document.querySelector('#ex2').addEventListener('click', function () {
+    document.querySelectorAll('p').forEach(element => {
+        element.style.color = "red"
+    });
+    
+})
+
+//Punkt 3
+document.querySelector('#ex3').addEventListener('click', function () {
+    document.querySelectorAll('li').forEach(element => {
+        element.style.rotate = "25deg"
+    });
+       
+})
+
+//Punkt 4
+document.querySelector('#ex4').addEventListener('click', function () {
+    document.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(element => {
+    element.style.display = "none"
+   });
+})
+
+
+document.querySelector('#ex5').addEventListener('click', function () {
+    
+})
+
+
+document.querySelector('#ex6').addEventListener('click', function () {
+    
+})
+
+
+document.querySelector('#ex7').addEventListener('click', function () {
+    
+})
+
+
+document.querySelector('#ex8').addEventListener('click', function () {
+    
+})
+
+
+document.querySelector('#ex9').addEventListener('click', function () {
+    
+})
+
+
+document.querySelector('#ex10').addEventListener('click', function () {
+    
+})
+
+
+document.querySelector('#ex11').addEventListener('click', function () {
+    
+})
+
+
+document.querySelector('#ex12').addEventListener('click', function () {
+    
+})
+
+
+document.querySelector('#ex13').addEventListener('click', function () {
+    
+})
